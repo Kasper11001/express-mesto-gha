4389dc984f552548fcb12a35a89ad893b90d33bb
Василий Васильев <k4sper-93@yandex.ru>
2022-04-27 02:10:43 +0300

    13pr-1st

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
   },
   extends: [
     'airbnb-base',
   ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ['react'],
   rules: {
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
   },
