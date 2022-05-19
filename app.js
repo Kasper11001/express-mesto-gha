b202f10ef3145eea3e564687855589f629054f96
unknown <k4sper-93@yandex.ru>
2022-05-19 23:10:43 +0300

    1111

--- app.js
+++ app.js
@@ -29,16 +29,8 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
-const allowedCors = [
-  'https://vasilyvasiliev.nomoreparties.sbs',
-];
-
 app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', '*');
 
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
